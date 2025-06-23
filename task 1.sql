bcde376469b77df7b2fcd621a31e60323b8b169c
Aniket Khandade <83821549+aniketkhandade9@users.noreply.github.com>
2025-06-23 12:42:29 +0530

    Add files via upload

---
+++ task 1.sql
@@ -0,0 +1,9 @@
+CREATE database railwayg;
+USE railwayg ;
+CREATE TABLE TICKET_detail (id INT auto_increment primary key , 
+Name1 varchar(100) NOT NULL, 
+Class varchar(100));
+CREATE Schema RAILWAY;
+Create TABLE RAILWAY.TICKET_detail(id INT auto_increment primary key ,
+Name1 varchar(100) not null,
+Class varchar (100));
